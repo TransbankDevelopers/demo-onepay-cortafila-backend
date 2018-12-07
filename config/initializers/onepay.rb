90e9e07a93452d183ecf76cb63c69cd128c6af66
Rodrigo Ayala <rayalac@gmail.com>
2018-12-07 16:41:57 -0300

    Use environment variables to set private data

--- config/initializers/onepay.rb
+++ config/initializers/onepay.rb
@@ -1,5 +1,5 @@
-Transbank::Onepay::Base.callback_url = "http://fa6e3983.ngrok.io/transaction/endTransaction"
-Transbank::Onepay::Base.api_key = "v4P4RD0fCmpIyR9GE1xbcgMWogU1UG79VBTne0Esx2c"
-Transbank::Onepay::Base.shared_secret = "!YE@_1G4V_6XL6SV26IN*PP1LB2?9CVQ"
+Transbank::Onepay::Base.callback_url = "#{ENV["BASE_DOMAIN"] || "https://cortafilas-onepay.herokuapp.com"}/transaction/endTransaction"
+Transbank::Onepay::Base.api_key = ENV["API_KEY"]
+Transbank::Onepay::Base.shared_secret = ENV["SHARED_SECRET"]
 
-Transbank::Onepay::Base.integration_type = :LIVE
+Transbank::Onepay::Base.integration_type = :LIVE if ENV["API_KEY"]
