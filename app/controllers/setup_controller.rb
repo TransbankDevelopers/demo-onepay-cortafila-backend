5e44e22d8d1f694ddd99c64d62cc5eeadf3edde2
Rodrigo Ayala <rayalac@gmail.com>
2018-12-17 15:35:22 -0300

    Fix device token update

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,12 +2,9 @@ class SetupController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def register
-        device = Device.find_or_create_by(deviceid: params[:deviceid]) do |device|
-            device.update_attributes(fcmtoken: params[:token])
-        end
+        device = Device.find_or_create_by(deviceid: params[:deviceid])
+        result = device.update(fcmtoken: params[:token]) ? "ok" : "error"
 
-        device.update(fcmtoken: params[:token])
-
-        render json: { "result": "ok" }
+        render json: { "result": result }
     end
 end
