be3dc36ad82ea9c9aa232bc6bcaa5a8a5c8af146
Rodrigo Ayala <rayalac@gmail.com>
2018-12-18 00:28:13 -0300

    Group ruby instructions

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -2,18 +2,14 @@ class TransactionController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        device = Device.find_by_deviceid(params[:deviceid])
-        items = params[:items].as_json.map {|item| Item.new(item)}
-
         cart = Transbank::Onepay::ShoppingCart.new params[:items].as_json
-
         channel = Transbank::Onepay::Channel::MOBILE
 
         @transaction_creation_response = Transbank::Onepay::Transaction.create(shopping_cart: cart, channel: channel).to_h
 
+        device = Device.find_by_deviceid(params[:deviceid])
+        items = params[:items].as_json.map {|item| Item.new(item)}
         ShoppingCart.create(device: device, items: items, ott: @transaction_creation_response["ott"], occ: @transaction_creation_response["occ"], amount: cart.total, external_unique_number: @transaction_creation_response["external_unique_number"])
-
-        logger.info "Cart total: #{cart.total}"
         
         render json: {
             responseCode: @transaction_creation_response["response_code"],
