3e97a46fcdf826c7068946614ef4d8798ac4cde8
Rodrigo Ayala <rayalac@gmail.com>
2018-12-28 18:01:27 -0300

    Fix file indentation

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,9 +2,9 @@ class SetupController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def register
-        device = Device.find_or_create_by(deviceid: params[:deviceid])
-        result = device.update(fcmtoken: params[:token]) ? "ok" : "error"
+      device = Device.find_or_create_by(deviceid: params[:deviceid])
+      result = device.update(fcmtoken: params[:token]) ? "ok" : "error"
 
-        render json: { "result": result }
+      render json: { "result": result }
     end
 end
