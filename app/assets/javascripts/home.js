73177ba3fada960248321f9cf569fa57bae67f31
Rodrigo Ayala <rayalac@gmail.com>
2019-01-02 22:10:08 -0300

    Show main menu with 4 items to select

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,34 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('#pay-button').click(function() {
+    $('#paymentModal').modal();
+  });
+
+  $('#paymentModal').on('show.bs.modal', function(e) {
+    new QRCode(document.getElementById("payment-qr"), {
+      text: "onepay=ott:12345678",
+      width: 150,
+      height: 150,
+      colorDark : "#000000",
+      colorLight : "#ffffff",
+      correctLevel : QRCode.CorrectLevel.H
+    });
+  });
+
+  $('#paymentModal').on('hidden.bs.modal', function(e) {
+    $('#payment-qr').empty();
+  });
+
+  $('.checkbox-item').change(function(e) {
+    let totalItem = $('#totalItems');
+    let price = $(this).data("price")
+
+    if (this.checked) {
+      totalItem.text(parseInt(totalItem.text()) + parseInt(price));
+    } else {
+      totalItem.text(parseInt(totalItem.text()) - parseInt(price));
+    }
+  });
+});
