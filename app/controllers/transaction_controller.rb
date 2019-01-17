44d95b4e50aae90fa84cec94567d767e9c7eb843
Rodrigo Ayala <rayalac@gmail.com>
2019-01-17 15:35:11 -0300

    Undo the string escaping of token

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -31,7 +31,7 @@ class TransactionController < ApplicationController
 
       shopping_cart = ShoppingCart.find_by_occ @occ
       device = shopping_cart.device
-      registration_ids = [device.fcmtoken.undump]
+      registration_ids = [device.fcmtoken]
       @items = shopping_cart.items
 
       options = { "data": {
